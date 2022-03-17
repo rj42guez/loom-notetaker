85c0e8e546c72276b97f41965b0d48320e3ff333
asopungco <andrew.sopungco@gmail.com>
2022-03-18 02:38:54 +0800

    Added Save button and successfully saved dialog

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from kivy.uix.widget import Widget
 # from tkinter import *
 from tkinter import Tk
 from tkinter import filedialog
+from tkinter import messagebox
 import os
+
+# current: variable storing currently opened file path, to be accessed across functions
+global current
+current = False
 
 class FileChoose(Popup):
     load = ObjectProperty()
@@ -30,6 +35,8 @@ class MyGridLayout(Widget):
         path = filedialog.askopenfilename()
         self.file_path = path
         if path:
+            global current
+            current = path
             file = open(path, 'r')
             contents = file.read()
             file.close()
@@ -42,13 +49,31 @@ class MyGridLayout(Widget):
         if path:
             file = open(path, 'w')
             file.write(self.ids.text_input.text)
+            messagebox.showinfo("Save As", "File successfully saved.")
 
 
     def new(self):
         # first clear contents
         self.ids.text_input.text = ""
+
+        # reset current to FALSE so that save will trigger as saveAs
+        global current
+        current = False
+
         # functionally the same as saveAs
         self.saveAs()
+
+    def save(self):
+        # checks whether a previously saved file is currently open
+        global current
+        if current:
+            file = open(current, 'w')
+            file.write(self.ids.text_input.text)
+            file.close()
+            messagebox.showinfo("Save", "File successfully saved.")
+        else:
+            self.saveAs()
+
 
 
 
