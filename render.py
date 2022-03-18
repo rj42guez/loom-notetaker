4c9c7054585e10dd50073b72207e1dce902a416e
aaaaaaaaa <steelfairytype@pm.me>
2022-03-18 11:59:34 +0800

    replacement functions

--- render.py
+++ render.py
@@ -1,4 +1,5 @@
-def process_bold_italics(candidate):
+import re
+
     # markdown formatting is as follows: 
     # **bold**
     # *italics*
@@ -9,5 +10,32 @@ def process_bold_italics(candidate):
 
     # candidate is a string
 
-    # regex to detect bold is /(\*\*|__)(.*?)\1/
-    # regex to detect italix is /(\*|_)(.*?)\1/
+    # the following needs tinkering to deal with edge cases
+
+    # '/(\*\*|__)(.*?)\1/' => '[b]\2[/b]',            // bold
+    # '/(\*|_)(.*?)\1/' => '[i]\2[/i]',                       // emphasis
+
+def process_bold(candidate):
+    return re.sub('(\*\*|__)(.*?)\1', '[b]\2[/b]', candidate, 2)
+
+def process_italics(candidate):
+    return re.sub('(\*|_)(.*?)\1', '[i]\2[/i]', candidate, 2)
+
+    # the following is taken from Slimdown, but it needs further editing as some of these don't work yet
+
+    # '/(#+)(.*)/' => 'bigger font',                           // headers
+    # '/\[([^\[]+)\]\(([^\)]+)\)/' => 'link in kivy?',  // links
+    # '/\~\~(.*?)\~\~/' => '<del>\1</del>',                     // del
+    # '/\:\"(.*?)\"\:/' => '<q>\1</q>',                         // quote
+    # '/`(.*?)`/' => '<code>\1</code>',                         // inline code
+    # '/\n\*(.*)/' => 'self::ul_list',                          // ul lists
+    # '/\n[0-9]+\.(.*)/' => 'self::ol_list',                    // ol lists
+    # '/\n(&gt;|\>)(.*)/' => 'self::blockquote ',               // blockquotes
+    # '/\n-{5,}/' => "\n<hr />",                                // horizontal rule
+
+# testing 
+
+candidate = "this should be **bold**"
+candidate2 = "this should be *italics*"
+
+
